8b976884ae005509ea201813886d16dd7d5064d6
Ricardo Aielo <ricardo.aielo@gmail.com>
2020-03-04 01:36:36 +0000

    Demo: hooks logging tied to 'demo' command

--- examples/demo/CoreHooksDemo.js
+++ examples/demo/CoreHooksDemo.js
@@ -2,10 +2,9 @@
 
 class CoreHooksDemo {
   constructor(serverless) {
+    this.sls = serverless;
     const self = this;
     const triggers = [
-      "before:core:init",
-      "before:core:init",
       "before:core:run",
       "before:core:cli:setloadedplugins",
       "before:core:pluginmanager:loadallplugins",
@@ -38,32 +37,48 @@ class CoreHooksDemo {
       }
     }
     self.hooks["demo:command"] = self.command.bind(self);
+    self.hooks["before:core:init"] = self.enable.bind(self);
     self.load();
   }
 
   command() {
-    let message = ">> Hi, I'm a command!";
+    let message = "Hi, I'm a command!";
     message += " Checkout what triggered before and after me.";
-    console.log(message);
-    message = ">> Try setting SLS_CH_DEBUG=1 to see debugging info.";
-    console.log(message);
+    this.log(message);
+    message = "Try setting SLS_CH_DEBUG=1 to see debugging info.";
+    this.log(message);
   }
 
-  load() {
-    let message = ">> Hello, I'm an instruction during plugin load.";
-    message += " See how many times I actually ran. (hint: 2)";
-    console.log(message);
+  enable() {
+    if (this.sls.processedInput.commands.includes("demo")) {
+      this.sls._demoEnabled = true;
+    } else {
+      this.log("Demo is active but hooks output is suppressed.");
+      this.log("Use the 'demo' command to check it out!");
+    }
   }
 
   hook(trigger, ...args) {
-    let message = "Hook triggered: " + trigger + " with ";
+    let message = "Core hook triggered: " + trigger + " with ";
     if (args.length == 0) {
       message += "no args";
     } else {
       message += "args " + JSON.stringify(args);
     }
     message = message.replace(/[\r\n\t]/g, "");
-    console.log(message);
+    if (this.sls._demoEnabled) {
+      console.log(">> " + message);
+    }
+  }
+
+  load() {
+    let message = "Hello, I'm an instruction in the plugin load.";
+    message += " See how many times I ran. (expected: 2)";
+    this.log(message);
+  }
+
+  log(message) {
+    this.sls.cli.log("[Demo] " + message);
   }
 }
 
