08ffd6aeef39ad1e57a3e112d899a8dedee6a4e4
Ricardo Aielo <ricardo.aielo@gmail.com>
2020-03-09 19:59:46 -0700

    serverless removed from dev dependencies

--- resources/vagrant/provision.sh
+++ resources/vagrant/provision.sh
@@ -12,17 +12,6 @@ fi
 apt-get update -y
 apt-get install git vim curl wget tree -y
 
-# Workaround to avoid symlink issues on Windows hosts
-MODULES_DIR="/home/vagrant/serverless-core-hooks/node_modules"
-YARNRC_FILE="/vagrant/.yarnrc"
-su - vagrant << WINHOSTFIX
-printf '\nexport NODE_PATH="\$NODE_PATH:$MODULES_DIR"' >> ~/.bashrc
-printf '\nexport PATH="\$PATH:$MODULES_DIR/.bin"\n' >> ~/.bashrc
-if [ ! -f $YARNRC_FILE ] || ! grep -Fq "modules-folder " $YARNRC_FILE; then
-  echo '--*.modules-folder "$MODULES_DIR"' >> $YARNRC_FILE
-fi
-WINHOSTFIX
-
 # VS Code settings (only if they do not exist; no replacement)
 VSCODE_DIR="/vagrant/.vscode"
 su - vagrant << VSCODE
@@ -36,7 +25,7 @@ su - vagrant << NODE
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install v12.16.0
-npm install yarn np@5.2 -g
+npm install yarn np@5.2 serverless -g
 cd /vagrant; yarn install
 NODE
 
